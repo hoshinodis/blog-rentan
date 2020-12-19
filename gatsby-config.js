b3cdb2dd1dd7cc59ccd5db458899a2030091b9e8
hoshinodis <okonomisentai@gmail.com>
2020-12-19 14:24:13 +0900

    chore

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,8 +79,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Foundation`,
-        short_name: `Foundation`,
+        name: `Rentan`,
+        short_name: `Rentan`,
         start_url: `/`,
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
--- src/content/pages/index.md
+++ src/content/pages/index.md
@@ -1,12 +1,11 @@
 ---
 template: index-page
 slug: /
-title: がんばれ！うふ子
+title: 競馬予想するやつ
 tagline: Data science taste Engeneer
-featuredImage: /assets/kaeruu.png
+featuredImage: /assets/rentan_logo.png
 cta:
   ctaText: Know more
   ctaLink: /about
 ---
 めっちゃがんばって予想するかんね！  
-→の絵は関係ないよ
